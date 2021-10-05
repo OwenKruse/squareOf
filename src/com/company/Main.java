a574879106e1cb7530884ea81faa96c1ab0651d4
OwenKruse <kruse.owen@vashonsd.org>
2021-10-05 10:51:10 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println(squareOf(4));
+    }
+    public static int squareOf(int N){
+        return N * N;
+    }
+}
